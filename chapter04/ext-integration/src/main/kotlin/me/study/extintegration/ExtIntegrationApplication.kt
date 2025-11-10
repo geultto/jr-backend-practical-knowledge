2f3d665b1ab53c9add1605a5baa1b39680b60802
JinseongHwang <jinseong.dev@gmail.com>
2025-11-11 01:36:25 +0900

    chore: init project

---
+++ chapter04/ext-integration/src/main/kotlin/me/study/extintegration/ExtIntegrationApplication.kt
@@ -0,0 +1,11 @@
+package me.study.extintegration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExtIntegrationApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExtIntegrationApplication>(*args)
+}
